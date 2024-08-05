ae2030e3465a87dba837904f8e9b7f99b0e25aad
flawstick <amirashak101@gmail.com>
2024-08-05 19:29:44 +0300

    Mongo bad URL issue fixed

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,11 +6,8 @@ dotenv.config();
 export const config = {
   port: process.env.PORT || 8080,
   jwtSecret: process.env.JWT_SECRET || "jwt-secret-key",
-  mongoUri:
-    `mongodb://${process.env.MONGODB_ADMIN_USERNAME}:${
-      process.env.MONGODB_ADMIN_PASSWORD
-    }@${process.env.MONGODB_URI || "localhost:27017"}` || "weaviate",
-  mongoOptions: { dbName: process.env.MONGODB_DB_NAME || "weaviate" },
+  mongoUri: `mongodb+srv://${process.env.MONGODB_ADMIN_USERNAME}:${process.env.MONGODB_ADMIN_PASSWORD}@${process.env.MONGODB_URI}`,
+  mongoOptions: { dbName: process.env.MONGODB_DB_NAME },
 };
 
 cloudinary.config({

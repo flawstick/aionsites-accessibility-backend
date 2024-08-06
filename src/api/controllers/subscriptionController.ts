18adff5994ac6ab213e1e78087068999d7a201a5
flawstick <amirashak101@gmail.com>
2024-08-06 16:53:08 +0300

    Adding other methods to subscriptions

--- src/api/controllers/subscriptionController.ts
+++ src/api/controllers/subscriptionController.ts
@@ -30,3 +30,45 @@ export async function getSubscriptions(
     res.status(400).json({ error: (error as Error).message });
   }
 }
+
+export async function deleteSubscription(
+  req: Request,
+  res: Response,
+): Promise<void> {
+  try {
+    await AccessiBeLicenseModel.findByIdAndDelete(req.params.id);
+    log.info(`Subscription deleted successfully by ${req.body.user.email}`);
+    res.status(200).json({ message: "Subscription deleted successfully" });
+  } catch (error) {
+    log.error(error as string);
+    res.status(400).json({ error: (error as Error).message });
+  }
+}
+
+export async function updateSubscription(
+  req: Request,
+  res: Response,
+): Promise<void> {
+  try {
+    await AccessiBeLicenseModel.findByIdAndUpdate(req.params.id, req.body);
+    log.info(`Subscription updated successfully by ${req.body.user.email}`);
+    res.status(200).json({ message: "Subscription updated successfully" });
+  } catch (error) {
+    log.error(error as string);
+    res.status(400).json({ error: (error as Error).message });
+  }
+}
+
+export async function getSubscription(
+  req: Request,
+  res: Response,
+): Promise<void> {
+  try {
+    const subscription = await AccessiBeLicenseModel.findById(req.params.id);
+    log.info(`Subscription retrieved successfully for ${req.body.user.email}`);
+    res.status(200).json(subscription);
+  } catch (error) {
+    log.error(error as string);
+    res.status(400).json({ error: (error as Error).message });
+  }
+}
--- src/api/routes/subscriptionRoutes.ts
+++ src/api/routes/subscriptionRoutes.ts
@@ -2,11 +2,17 @@ import express from "express";
 import {
   createSubscription,
   getSubscriptions,
+  deleteSubscription,
+  updateSubscription,
+  getSubscription,
 } from "@/api/controllers/subscriptionController";
 
 const router = express.Router();
 
 router.post("/", createSubscription);
 router.get("/", getSubscriptions);
+router.delete("/:id", deleteSubscription);
+router.put("/:id", updateSubscription);
+router.get("/:id", getSubscription);
 
 export default router;

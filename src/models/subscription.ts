0f7f1bba1fdf6a891f9e698baf3790f832c4b458
flawstick <amirashak101@gmail.com>
2024-08-06 17:17:25 +0300

    Adding email to type, making unstrict.

--- src/models/subscription.ts
+++ src/models/subscription.ts
@@ -42,41 +42,45 @@ interface IAccessiBeLicenseBase {
 interface IAccessiBeLicense extends IAccessiBeLicenseBase, Document {}
 export interface IAccessiBeLicenseLean extends IAccessiBeLicenseBase {}
 
-const AccessiBeLicenseSchema = new Schema<IAccessiBeLicense>({
-  companyName: { type: String, required: true },
-  addresses: { type: [String], required: true },
-  location: {
-    longitude: { type: Number },
-    latitude: { type: Number },
-  },
-  domain: { type: String, required: true },
-  CName: { type: String },
-  website: { type: String, required: true },
-  contactPersonName: { type: String, required: true },
-  phoneNumber: { type: String },
-  licenseKey: { type: String },
-  widgetInstallationProcedureType: { type: String },
-  active: { type: Boolean, required: true },
-  expirationDate: { type: Date, required: true },
-  subscriptionCard: {
-    cardNumber: { type: String, required: true },
-    cardHolderName: { type: String, required: true },
+const AccessiBeLicenseSchema = new Schema<IAccessiBeLicense>(
+  {
+    companyName: { type: String, required: true },
+    addresses: { type: [String], required: true },
+    email: { type: String, required: true },
+    location: {
+      longitude: { type: Number },
+      latitude: { type: Number },
+    },
+    domain: { type: String, required: true },
+    CName: { type: String },
+    website: { type: String, required: true },
+    contactPersonName: { type: String, required: true },
+    phoneNumber: { type: String },
+    licenseKey: { type: String },
+    widgetInstallationProcedureType: { type: String },
+    active: { type: Boolean, required: true },
     expirationDate: { type: Date, required: true },
-    cvv: { type: String, required: true },
-    cardType: { type: String, required: true },
+    subscriptionCard: {
+      cardNumber: { type: String, required: true },
+      cardHolderName: { type: String, required: true },
+      expirationDate: { type: Date, required: true },
+      cvv: { type: String, required: true },
+      cardType: { type: String, required: true },
+    },
+    subscriptionType: { type: String, required: true },
+    subscriptionTier: { type: String, required: true },
+    subscriptionStatus: { type: String, required: true },
+    subscriptionStartDate: { type: Date, required: true },
+    subscriptionEndDate: { type: Date, required: true },
+    subscriptionRenewalDate: { type: Date, required: true },
+    subscriptionRenewalPrice: { type: Number, required: true },
+    subscriptionRenewalCurrency: { type: String, required: true },
+    subscriptionRenewalPeriod: { type: String, required: true },
+    subscriptionRenewalPeriodUnit: { type: String, required: true },
+    subscriptionRenewalPeriodFrequency: { type: Number, required: true },
   },
-  subscriptionType: { type: String, required: true },
-  subscriptionTier: { type: String, required: true },
-  subscriptionStatus: { type: String, required: true },
-  subscriptionStartDate: { type: Date, required: true },
-  subscriptionEndDate: { type: Date, required: true },
-  subscriptionRenewalDate: { type: Date, required: true },
-  subscriptionRenewalPrice: { type: Number, required: true },
-  subscriptionRenewalCurrency: { type: String, required: true },
-  subscriptionRenewalPeriod: { type: String, required: true },
-  subscriptionRenewalPeriodUnit: { type: String, required: true },
-  subscriptionRenewalPeriodFrequency: { type: Number, required: true },
-});
+  { timestamps: true, strict: false },
+);
 
 export const AccessiBeLicenseModel: Model<IAccessiBeLicense> =
   model<IAccessiBeLicense>("AccessiBeLicense", AccessiBeLicenseSchema);
